54721e41f24d6cda4111e619d759e0f0c17c6f9b
Sylvain L <7266910+sylvain-lec@users.noreply.github.com>
2024-01-30 17:51:54 +0100

    Commentaires, liste de TODO et lien type MIME ajoutés

--- src/serveur/AfficheRequetesHttp.java
+++ src/serveur/AfficheRequetesHttp.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 public class AfficheRequetesHttp {
 
 	public static void main(String[] args) {
-		// TEEEEEESSSSSTTTTT
 		if (args.length != 2) {
 			System.err.println("Usage: java " + AfficheRequetesHttp.class.getName() + "portnumber directory");
 			//il faut pas pouvoir remonter plus loin que le dir du serveur. pb de sécu
@@ -26,14 +25,16 @@ public class AfficheRequetesHttp {
 			System.err.println("Usage: java " + AfficheRequetesHttp.class.getName() + "portnumber directory");
 			System.exit(2);
 		}
-
+		// écouter sur port portNumber
 		try (ServerSocket s = new ServerSocket(portNumber);) {
-
+			// TANT QUE (vrai) FAIRE
 			while (true) {
-
+				// accepter connexion
 				try (Socket c = s.accept();) {
+					// récupérer flux d'entrée
 					Scanner sc = new Scanner(c.getInputStream());
 					System.out.println("----DEBUT REQUETE----");
+					// lire la première ligne sur flux d'entrée
 					while (sc.hasNextLine()) {
 						String line = sc.nextLine();
 						// on sort de la boucle si on arrive sur la ligne vide de fin des headers
@@ -43,36 +44,46 @@ public class AfficheRequetesHttp {
 						System.out.println("-" + line + "-");
 					}
 					System.out.println("----FIN REQUETE----");
-
-					OutputStream os = c.getOutputStream();
-
-					// exemple d'envoi d'un contenu html
-					/*Writer w = new OutputStreamWriter(os);
-					PrintWriter pw = new PrintWriter(w);
-					pw.println("HTTP/1.1 200 OK");
-					pw.println("Content-Type: text/html");
-					pw.println();// ligne vide pour signaler la fin des entÃªtes 
-					//CORPS DE LA REQUETE : (sera le code source de la ressources envoyée)
-					pw.println("<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\">");
-					pw.println("<html><body>");
-					pw.println("<h1>Hello DCISS !</h1>");
-					pw.println("</body></html>");
-					pw.flush(); // il faut vider le buffer pour que le contenu soit envoyÃ©.
-					*/
 					
-					// exemple d'envoi de fichier
-					Path fileToSend = Paths.get("image.png");
-					String mimeType = Files.probeContentType(fileToSend);
-
-					Writer w = new OutputStreamWriter(os);
-					PrintWriter pw = new PrintWriter(w);
-					pw.println("HTTP/1.1 200 OK");
-					pw.println("Content-Type: " + mimeType);
-					pw.println();// ligne vide pour signaler la fin des entÃªtes
-					pw.flush();// il faut vider le buffer pour que le contenu soit envoyÃ©. envoie à OutputStream os je crois
-
-					Files.copy(fileToSend, os); // ici pas besoin de vider le buffer os n'est pas buffeurisÃ©
-
+					// récupérer flux de sortie
+					OutputStream os = c.getOutputStream();
+					
+					// TODO: SI (méthode get) ALORS
+					
+						// TODO: récupérer le chemin dans la première ligne
+					
+						// TODO: SI (CHEMIN est un répertoire) ALORS
+							// construire réponse affichant le contenu du répertoire
+							// exemple d'envoi d'un contenu html
+							Writer w = new OutputStreamWriter(os);
+							PrintWriter pw = new PrintWriter(w);
+							pw.println("HTTP/1.1 200 OK");
+							pw.println("Content-Type: text/html");
+							pw.println();// ligne vide pour signaler la fin des entêtes 
+							//CORPS DE LA REQUETE : (sera le code source de la ressources envoyée)
+							pw.println("<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\">");
+							pw.println("<html><body>");
+							pw.println("<h1>Hello DCISS !</h1>");
+							pw.println("</body></html>");
+							pw.flush(); // il faut vider le buffer pour que le contenu soit envoyé.
+					
+						// TODO: SINON SI (CHEMIN est un fichier)
+							// construire réponse avec le contenu du fichier
+							// exemple d'envoi de fichier
+							Path fileToSend = Paths.get("image.png");
+							String mimeType = Files.probeContentType(fileToSend);
+							// Infos sur types MIME = https://developer.mozilla.org/fr/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+							Writer w = new OutputStreamWriter(os);
+							PrintWriter pw = new PrintWriter(w);
+							pw.println("HTTP/1.1 200 OK");
+							pw.println("Content-Type: " + mimeType);
+							pw.println();// ligne vide pour signaler la fin des entêtes
+							pw.flush();// il faut vider le buffer pour que le contenu soit envoyé. envoie à OutputStream os je crois
+		
+							Files.copy(fileToSend, os); // ici pas besoin de vider le buffer os n'est pas buffeurisé
+						// SINON
+							// envoyer erreur 404
+					
 				}
 
 			}
