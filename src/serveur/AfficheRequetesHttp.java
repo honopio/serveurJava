6fc7984f77deebdf98d4a6c3d787986379d0e022
Sylvain L <7266910+sylvain-lec@users.noreply.github.com>
2024-02-06 16:06:41 +0100

    WIP if folder/files

--- src/serveur/AfficheRequetesHttp.java
+++ src/serveur/AfficheRequetesHttp.java
@@ -38,7 +38,7 @@ public class AfficheRequetesHttp {
 			}
 		} catch (NumberFormatException e) {
 			System.err.println(args[0] + " is not an integer");
-			System.err.println("Usage: java " + AfficheRequetesHttp.class.getName() + "portnumber directory");
+			System.err.println("Usage: java " + AfficheRequetesHttp.class.getName() + " portnumber directory");
 			System.exit(4);
 		}
 		// écouter sur port portNumber
@@ -50,6 +50,9 @@ public class AfficheRequetesHttp {
 					// récupérer flux d'entrée
 					Scanner sc = new Scanner(c.getInputStream());
 					System.out.println("----DEBUT REQUETE----");
+					// stocker la premiere ligne pour type de requête
+					String firstline = sc.nextLine();
+					System.out.println(firstline);
 					// lire la première ligne sur flux d'entrée
 					while (sc.hasNextLine()) {
 						String line = sc.nextLine();
@@ -64,16 +67,14 @@ public class AfficheRequetesHttp {
 					// récupérer flux de sortie
 					OutputStream os = c.getOutputStream();
 					
-					// TODO: SI (méthode get) ALORS
-					/*ouvrir un nv scanner juste pour lire le premier mot ? la String line contient la dernière ligne du header, pas la premiere
-					 * -> Scanner sca = new Scanner(c.getInputStream()); if ("GET".equals(scanner.next())) {}
-					 * sinon, stocker la premiere ligne (dans une String firstLine) et la print avant le while, puis print le reste du header dans le while
-					 * -> if (firstLine.substring(0, 3).equals("GET")) {}
-					 */
+					// SI (méthode get) ALORS
+					if (firstline.substring(0, 3) == "GET") {
 					
-						// TODO: récupérer le chemin dans la première ligne
+						// récupérer le chemin dans la première ligne
+						Path chemin = Paths.get(firstline.split(" ")[1]);
 					
 						// TODO: SI (CHEMIN est un répertoire) ALORS
+						if (Files.isDirectory(chemin)) {
 							// construire réponse affichant le contenu du répertoire
 							// exemple d'envoi d'un contenu html
 							Writer w = new OutputStreamWriter(os);
@@ -87,26 +88,30 @@ public class AfficheRequetesHttp {
 							pw.println("<h1>Hello DCISS !</h1>");
 							pw.println("</body></html>");
 							pw.flush(); // il faut vider le buffer pour que le contenu soit envoyé.
-					
+						}
 						// TODO: SINON SI (CHEMIN est un fichier)
+						else if (Files.isRegularFile(chemin)) {
 							// construire réponse avec le contenu du fichier
 							// exemple d'envoi de fichier
-							Path fileToSend = Paths.get("image.png");
-							String mimeType = Files.probeContentType(fileToSend);
+							String mimeType = Files.probeContentType(chemin);
 							// Infos sur types MIME = https://developer.mozilla.org/fr/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
-							Writer w2 = new OutputStreamWriter(os);
-							PrintWriter pw2 = new PrintWriter(w);
+							Writer w = new OutputStreamWriter(os);
+							PrintWriter pw = new PrintWriter(w);
 							pw.println("HTTP/1.1 200 OK");
 							pw.println("Content-Type: " + mimeType);
 							pw.println();// ligne vide pour signaler la fin des entêtes
 							pw.flush();// il faut vider le buffer pour que le contenu soit envoyé. envoie à OutputStream os je crois
 		
 							// Copier le contenu d'un ficher vers un flux en sortie
-							Files.copy(fileToSend, os); // ici pas besoin de vider le buffer os n'est pas buffeurisé
+							Files.copy(chemin, os); // ici pas besoin de vider le buffer os n'est pas buffeurisé
+						}
+						else {
 						// TODO: SINON
 							// envoyer erreur 404
+						}
 						// FIN SI
-						// TODO: fermer la connexion. la classe ServerSocket a une methode close(). c.close(); */
+					}
+					// TODO: fermer la connexion. la classe ServerSocket a une methode close(). c.close(); */
 					// FIN TANT QUE
 					
 				}
